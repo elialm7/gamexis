16af2fefc2f97bad048558084dcc83dc130cf63d
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-13 17:44:22 -0300

    Comenté otra vez :)

--- core/src/main/java/gamexis/core/GameXis.java
+++ core/src/main/java/gamexis/core/GameXis.java
@@ -30,7 +30,7 @@ public class GameXis implements ApplicationListener {
 		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
 		batch.end();
 	}
-
+//Hola
 	@Override
 	public void pause () {
 	}

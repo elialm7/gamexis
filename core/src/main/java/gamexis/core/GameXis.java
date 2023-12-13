3315b86bbb175ac8e0b661b64cf03f701c0d5cc9
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-13 17:31:30 -0300

    Puse un comentario de prueba

--- core/src/main/java/gamexis/core/GameXis.java
+++ core/src/main/java/gamexis/core/GameXis.java
@@ -30,9 +30,10 @@ public class GameXis implements ApplicationListener {
 		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
 		batch.end();
 	}
-
+//Efectivamente sirve para pausar
 	@Override
 	public void pause () {
+
 	}
 
 	@Override

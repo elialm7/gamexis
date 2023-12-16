fc588db080f1386de20a296a3acad1bce7f7980e
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-16 16:16:48 -0300

    Creación y separación de clase Servidor a ServidorUDP para multigrupos en distintos hilos de proceso.

---
+++ servidor/src/main/java/org/gamexis/servidor/protocolos/ServidorUDP.java
@@ -0,0 +1,64 @@
+package org.gamexis.servidor.protocolos;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.MembershipKey;
+
+public class ServidorUDP implements Runnable {
+
+    private final DatagramChannel channel;
+    private final MembershipKey membershipKey;
+    private volatile boolean cerrado = false;
+
+
+    public ServidorUDP(int puerto, String direccionMulticast ) throws IOException {
+        channel = DatagramChannel.open(StandardProtocolFamily.INET)
+                .setOption(StandardSocketOptions.SO_REUSEADDR, true)
+                .bind(new InetSocketAddress(puerto));
+       // channel.configureBlocking(false);
+        InetAddress group = InetAddress.getByName(direccionMulticast);
+        NetworkInterface networkInterface = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+        System.out.println(InetAddress.getLocalHost());
+        membershipKey = channel.join(group, networkInterface);
+        System.out.println("Servidor Multicast UDP esperando mensajes en la dirección "+direccionMulticast+"...");
+    }
+
+    public void cerrar() throws IOException {
+        cerrado = !cerrado;
+        membershipKey.drop();
+        channel.close();
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true) {
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                SocketAddress clientAddress = channel.receive(buffer);
+
+                buffer.flip();
+                String message = new String(buffer.array(), 0, buffer.limit());
+
+                System.out.println("Mensaje recibido de " + clientAddress + ": " + message);
+
+                if (message.equals("cerrar")) {
+                    System.out.println("Cerrando el servidor...");
+
+                    break;
+                }
+                ByteBuffer enviarBuffer = ByteBuffer.wrap(message.getBytes());
+                channel.send(enviarBuffer,clientAddress);
+            }
+            System.out.println("Ya");
+            cerrar();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isCerrado() {
+        return cerrado;
+    }
+}

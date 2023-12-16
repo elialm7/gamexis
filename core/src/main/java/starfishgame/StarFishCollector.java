eb675d1cda415aff12f8ae799ea7815866995eb1
elikawa7 <metall65eli@gmail.com>
2023-12-16 20:27:42 -0300

    mejor implementacion de un game loop

--- core/src/main/java/starfishgame/StarFishCollector.java
+++ core/src/main/java/starfishgame/StarFishCollector.java
@@ -7,100 +7,53 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.compression.lzma.Base;
+import starfishgame.Actors.BaseActor;
+import starfishgame.Actors.Turtle;
+import starfishgame.GameLoop.GameLoop;
 
-public class StarFishCollector extends Game {
-
-
-    private SpriteBatch batch;
-
-    private Texture turtleTexture;
-    private float turtleX;
-    private float turtleY;
-    private Rectangle turtleRectangle;
-
-    private Texture starfishTexture;
-    private float starfishX;
-    private float starfishY;
-    private Rectangle starfishRectangle;
-
-    private Texture oceanTexture;
-    private Texture winMessageTexture;
-
-    private boolean win;
+public class StarFishCollector extends GameLoop {
 
 
-    private void clearScreen(){
-        Gdx.gl.glClearColor(0,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    }
-
-    private void handleInput(){
-
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            this.turtleX-=20;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            this.turtleX+=20;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            this.turtleY+=20;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            this.turtleY-=20;
-        }
-
-    }
-
-    private void updateTurtleLocation(){
-        this.turtleRectangle.setPosition(turtleX, turtleY);
-    }
-
-    private void checkWinningCondition(){
-
-        if(turtleRectangle.overlaps(starfishRectangle)){
-            win = true;
-        }
-
-    }
-
-    private void drawGraphics(){
-
-        this.batch.begin();
-        this.batch.draw(oceanTexture, 0,0);
-        if(!win){
-            this.batch.draw(starfishTexture,this.starfishX, this.starfishY);
-        }
-        this.batch.draw(turtleTexture, this.turtleX,this.turtleY);
-        if(win){
-            batch.draw(winMessageTexture, 180, 180);
-        }
-        batch.end();
-
-
-    }
+    private Turtle turtle;
+    private BaseActor starfish;
+    private BaseActor ocean;
+    private BaseActor winMessage;
+    private boolean winCondition;
     @Override
-    public void create() {
-        Gdx.app.log("Loading", "Cargando los datos para el juego");
-        this.batch = new SpriteBatch();
-        this.turtleTexture = new Texture(Gdx.files.internal("turtle-1.png"));
-        this.turtleX = 20;
-        this.turtleY = 20;
-        this.turtleRectangle = new Rectangle(this.turtleX, this.turtleY, this.turtleTexture.getWidth(),this.turtleTexture.getHeight());
-        this.starfishTexture = new Texture(Gdx.files.internal("starfish.png"));
-        this.starfishX = 380;
-        this.starfishY = 380;
-        this.starfishRectangle = new Rectangle(this.starfishX, this.starfishY, this.starfishTexture.getWidth(), this.starfishTexture.getHeight());
-        this.oceanTexture = new Texture(Gdx.files.internal("water.jpg"));
-        this.winMessageTexture = new Texture(Gdx.files.internal("you-win.png"));
-        this.win = false;
+    public void initialize() {
+        ocean = new BaseActor();
+        ocean.setTexture( new Texture( Gdx.files.internal("water.jpg") )  );
+        mainStage.addActor(ocean);
+
+        starfish = new BaseActor();
+        starfish.setTexture( new Texture(Gdx.files.internal("starfish.png")) );
+        starfish.setPosition( 380,380 );
+        mainStage.addActor( starfish );
+
+        turtle = new Turtle();
+        turtle.setTexture( new Texture(Gdx.files.internal("turtle-1.png")) );
+        turtle.setPosition( 20,20 );
+        mainStage.addActor( turtle );
+
+        winMessage = new BaseActor();
+        winMessage.setTexture( new Texture(Gdx.files.internal("you-win.png")) );
+        winMessage.setPosition( 180,180 );
+        winMessage.setVisible( false );
+        mainStage.addActor( winMessage );
+        winCondition = false;
     }
 
     @Override
-    public void render() {
-        handleInput();
-        updateTurtleLocation();
-        checkWinningCondition();
-        clearScreen();
-        drawGraphics();
+    public void update(float dt) {
+
+        if(turtle.overlaps(starfish)){
+
+            starfish.remove();
+            winMessage.setVisible(true);
+
+        }
+
+
     }
 }

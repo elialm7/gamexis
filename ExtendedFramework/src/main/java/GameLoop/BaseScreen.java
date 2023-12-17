b0e2ab6f6aaef6d92b899f37718ef260acd7355e
elikawa7 <metall65eli@gmail.com>
2023-12-17 20:53:42 -0300

    cada screen es basicamente su propio gameloop, de este se pueden crear niveles,  manus,etc.

--- ExtendedFramework/src/main/java/GameLoop/BaseScreen.java
+++ ExtendedFramework/src/main/java/GameLoop/BaseScreen.java
@@ -1,19 +1,21 @@
 package GameLoop;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public abstract class GameLoop extends Game {
+public abstract class BaseScreen implements Screen {
 
     protected Stage mainStage;
-    @Override
-    public void create() {
-        this.mainStage = new Stage();
+    protected Stage uiStage;
+    public BaseScreen(){
+        mainStage = new Stage();
+        uiStage = new Stage();
         initialize();
     }
-
+    public abstract void initialize();
+    public abstract void update(float dt);
     /*
      * The game loop consists of three stages.
      *(1) process input (act method).
@@ -21,8 +23,7 @@ public abstract class GameLoop extends Game {
      *(3) render the graphics
      * */
     @Override
-    public void render() {
-
+    public void render(float delta) {
         //get deltatime: the difference in time from the last iteration of the game loop, if we assume 60fps, then deltatime should be 1/60. not always though.
         float deltatime = Gdx.graphics.getDeltaTime();
         //run act method.
@@ -34,8 +35,19 @@ public abstract class GameLoop extends Game {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         // draw the graphics.
         this.mainStage.draw();
+        this.uiStage.act(deltatime);
+        this.uiStage.draw();
     }
-
-    public abstract void initialize();
-    public abstract void update(float dt);
+    @Override
+    public void resize(int width, int height) {}
+    @Override
+    public void show() {}
+    @Override
+    public void hide() {}
+    @Override
+    public void pause() {}
+    @Override
+    public void resume() {}
+    @Override
+    public void dispose() {}
 }

e6b188e2fc09f983dd32fe8f76fc24f9bb1e0d09
elikawa7 <metall65eli@gmail.com>
2023-12-17 17:34:07 -0300

    implementacion de las fisica al objeto que se mueve

--- core/src/main/java/starfishgame/Turtle.java
+++ core/src/main/java/starfishgame/Turtle.java
@@ -3,11 +3,22 @@ package starfishgame;
 import Actors.BaseActor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Turtle extends BaseActor {
 
-    public Turtle(){
-        super();
+
+
+    public Turtle(float x, float y, Stage s){
+        super(x, y, s);
+        String[] filenames = {
+                "turtle-1.png", "turtle-2.png","turtle-3.png", "turtle-4.png", "turtle-5.png", "turtle-6.png"
+        };
+        loadAnimationFromFiles(filenames, 0.1f, true);
+
+        setAcceleration(400);
+        setMaxSpeed(100);
+        setDeceleration(400);
     }
 
     @Override
@@ -15,16 +26,21 @@ public class Turtle extends BaseActor {
         super.act(dt);
 
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            this.moveBy(-15, 0);
+            accelerateAtAngle(180);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            this.moveBy(15, 0);
+           accelerateAtAngle(0);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            this.moveBy(0, 15);
+            accelerateAtAngle(90);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            this.moveBy(0, -15);
+           accelerateAtAngle(270);
+        }
+        applyPhysics(dt);
+        setAnimationPaused(!isMoving());
+        if(getSpeed()>0){
+            setRotation(getMotionAngle());
         }
 
     }

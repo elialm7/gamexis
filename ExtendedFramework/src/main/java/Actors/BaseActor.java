e73660b42ee5a87562fbed502971f8486b7d0848
elikawa7 <metall65eli@gmail.com>
2023-12-17 17:11:31 -0300

    aniadi soporte para velocidad.

--- ExtendedFramework/src/main/java/Actors/BaseActor.java
+++ ExtendedFramework/src/main/java/Actors/BaseActor.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
@@ -17,30 +18,51 @@ import java.util.Arrays;
 
 public class BaseActor extends Actor {
 
-    private TextureRegion region;
-    private Rectangle rectangle;
-
     private Animation animation;
     private float elapsedtime;
     private boolean animationPaused;
 
-    public BaseActor(){
-        super();
-        this.region = new TextureRegion();
-        this.rectangle = new Rectangle();
-
-        this.animation = null;
-        this.elapsedtime = 0;
-        this.animationPaused = false;
-    }
-
-    /**
-     *
-     */
+    private Vector2 velocityVec;
     public BaseActor(float x, float y, Stage s){
         super();
         setPosition(x, y);
         s.addActor(this);
+
+        this.animation = null;
+        this.elapsedtime = 0;
+        this.animationPaused = false;
+
+        this.velocityVec = new Vector2(0,0);
+
+
+    }
+
+    public void setSpeed(float speed){
+        if(velocityVec.len() == 0){
+            velocityVec.set(speed, 0);
+        }else{
+            /*
+            * primero normalizamos el vector ,
+            * es decir convertimos a su vector unitario,  y luego scalamos a la velocidad desea.
+            * */
+            velocityVec.nor().scl(speed);
+        }
+    }
+
+    public float getSpeed(){
+        return this.velocityVec.len();
+    }
+
+    public float getMotionAngle(){
+        return this.velocityVec.angle();
+    }
+
+    public void setMotionAngle(float angle){
+        this.velocityVec.setAngle(angle);
+    }
+
+    public boolean isMoving(){
+        return(getSpeed()>0);
     }
 
 
@@ -119,20 +141,10 @@ public class BaseActor extends Actor {
     }
 
 
-    public void setTexture(Texture t){
-        this.region.setRegion(t);
-        setSize(t.getWidth(), t.getHeight());
-        rectangle.setSize(t.getWidth(), t.getHeight());
 
-    }
-    public Rectangle getRectangle(){
-        this.rectangle.setPosition(getX(), getY());
-        return this.rectangle;
-    }
 
-    public boolean overlaps(BaseActor actor){
-        return this.getRectangle().overlaps(actor.getRectangle());
-    }
+
+
 
     public void act(float dt){
         super.act(dt);
@@ -148,7 +160,7 @@ public class BaseActor extends Actor {
         Color c = getColor();
         batch.setColor(c);
         if(animation!= null && isVisible()){
-            batch.draw(region, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+            batch.draw(animation.getKeyFrame(elapsedtime), getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
         }
     }
 }

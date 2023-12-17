472e9cc375f89e88bb42c26579f3dbbc16431be3
elikawa7 <metall65eli@gmail.com>
2023-12-17 14:21:10 -0300

    ahora puede cargar textures animados desde archivos, spritesheets o una imagen sola.

--- core/src/main/java/ExtendedFramework/Actors/BaseActor.java
+++ core/src/main/java/ExtendedFramework/Actors/BaseActor.java
@@ -1,5 +1,6 @@
 package ExtendedFramework.Actors;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 
@@ -9,6 +10,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Arrays;
 
 
 public class BaseActor extends Actor {
@@ -53,6 +57,67 @@ public class BaseActor extends Actor {
     public void setAnimationPaused(boolean pause){
         this.animationPaused = pause;
     }
+
+    public Animation loadAnimationFromFiles(String[] fileNames, float frameduration, boolean loop){
+        int fileCount = fileNames.length;
+        Array<TextureRegion> texturearray = new Array<>();
+        Arrays.stream(fileNames).map(filename -> new Texture(Gdx.files.internal(filename))).forEach(texture -> {
+            texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            texturearray.add(new TextureRegion(texture));
+        });
+
+        Animation anim = new Animation(frameduration, texturearray);
+
+        if(loop){
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        }else {
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+        }
+        if (this.animation == null){
+            setAnimation(anim);
+        }
+        return anim;
+    }
+
+    public Animation loadAnimationFromSheet(String filename, int rows, int cols, float frameDuration, boolean loop){
+
+        Texture texture = new Texture(Gdx.files.internal(filename));
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        int frameWidth = texture.getWidth()/cols;
+        int frameHeight = texture.getHeight()/rows;
+        TextureRegion[][] temp = TextureRegion.split(texture, frameWidth, frameHeight);
+
+        Array<TextureRegion> textureArray = new Array<>();
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                textureArray.add(temp[r][c]);
+            }
+        }
+        Animation anim = new Animation(frameDuration, textureArray);
+        if(loop){
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        }else{
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+        }
+        if(this.animation == null){
+            setAnimation(anim);
+        }
+
+        return anim;
+
+    }
+
+
+    public boolean isAnimationFinished(){
+        return this.animation.isAnimationFinished(elapsedtime);
+    }
+
+    public Animation loadTexture(String filename){
+        String[] fileNames = new String[1];
+        fileNames[0] = filename;
+        return loadAnimationFromFiles(fileNames, 1, true);
+    }
+
 
     public void setTexture(Texture t){
         this.region.setRegion(t);

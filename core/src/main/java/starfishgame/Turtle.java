144c9bd3633d670b60cbf53d2602a87fe1e04ca0
elikawa7 <metall65eli@gmail.com>
2023-12-17 21:01:32 -0300

    aniedi el bountoworld y la alineacion de la camara .

--- core/src/main/java/starfishgame/Turtle.java
+++ core/src/main/java/starfishgame/Turtle.java
@@ -15,10 +15,12 @@ public class Turtle extends BaseActor {
                 "turtle-1.png", "turtle-2.png","turtle-3.png", "turtle-4.png", "turtle-5.png", "turtle-6.png"
         };
         loadAnimationFromFiles(filenames, 0.1f, true);
-
+        setBoundaryPolygon(8);
         setAcceleration(400);
         setMaxSpeed(100);
         setDeceleration(400);
+
+
     }
 
     @Override
@@ -42,6 +44,9 @@ public class Turtle extends BaseActor {
         if(getSpeed()>0){
             setRotation(getMotionAngle());
         }
+
+        boundToWorld();
+        alignCamera();
 
     }
 }

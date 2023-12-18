cbf675e62a2675a087adc2a52bfeb6c8f82ee359
elikawa7 <metall65eli@gmail.com>
2023-12-17 21:01:57 -0300

    el whirpool.

---
+++ core/src/main/java/starfishgame/WhirlPool.java
@@ -0,0 +1,21 @@
+package starfishgame;
+
+import Actors.BaseActor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class WhirlPool extends BaseActor {
+
+    public WhirlPool(float x, float y, Stage s){
+        super(x, y, s);
+        loadAnimationFromSheet("whirlpool.png", 2, 5, 0.1f, false);
+
+    }
+
+    @Override
+    public void act(float dt) {
+        super.act(dt);
+        if(isAnimationFinished()){
+            remove();
+        }
+    }
+}

4411252e68781d88cadcba2e99f93396458eb0f9
elikawa7 <metall65eli@gmail.com>
2023-12-19 16:54:22 -0300

    logica del juego.

--- core/src/main/java/Space_Rocks/SpaceShip.java
+++ core/src/main/java/Space_Rocks/SpaceShip.java
@@ -3,6 +3,7 @@ package Space_Rocks;
 import Actors.BaseActor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class SpaceShip extends BaseActor {
@@ -58,6 +59,35 @@ public class SpaceShip extends BaseActor {
     }
 
 
+    public void warp(){
+        if(getStage() == null){
+            return;
+        }
+        Warp warp1 = new Warp(0, 0, this.getStage());
+        warp1.centerAtActor(this);
+        setPosition(MathUtils.random(800), (float) MathUtils.random(600));
+        Warp warp2 = new Warp(0,0, this.getStage());
+        warp2.centerAtActor(this);
+    }
 
+
+    public void shoot(){
+        if(getStage()== null){
+            return;
+        }
+        Laser laser = new Laser(0,0, this.getStage());
+        laser.centerAtActor(this);
+        laser.setRotation(this.getRotation());
+        laser.setMotionAngle(this.getRotation());
+
+    }
+
+    public int getShieldPower(){
+        return this.shieldpower;
+    }
+
+    public void setShieldPower(int power){
+        this.shieldpower = power;
+    }
 
 }

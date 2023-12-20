d42febc347f625c49c2364c4fbf3801d01a2e504
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-20 17:06:51 -0300

    Reestructuración completa de la clase Cliente.

--- cliente/src/main/java/org/gamexis/cliente/Cliente.java
+++ cliente/src/main/java/org/gamexis/cliente/Cliente.java
@@ -1,70 +1,41 @@
 package org.gamexis.cliente;
 
+
+import org.gamexis.cliente.protocolo.UDP;
+
 import java.io.IOException;
-import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
-import java.nio.charset.StandardCharsets;
 
-public class Cliente implements Runnable {
-    private static final int BUFFER_SIZE = 256;
-    private static final String MULTICAST_GROUP = "234.0.1.1";
+public class Cliente extends Thread implements ICliente, ClienteObserver{
+    private final int PUERTO_UDP = 8888;
+    private final int PUERTO_TCP = 3100;
 
-    private final DatagramChannel channel;
-    private final InetSocketAddress serverSocketAddress;
-    private final ByteBuffer bufferEmisor;
-    private final ByteBuffer bufferReceptor;
+    private final UDP UDP;
 
-    public Cliente(String servidorDireccion) throws IOException {
-        channel = DatagramChannel.open();
-        channel.configureBlocking(false);
-        InetAddress serverAddress = InetAddress.getByName(servidorDireccion);
-        serverSocketAddress = new InetSocketAddress(serverAddress, 8888);
-        bufferEmisor = bufferReceptor = ByteBuffer.allocate(BUFFER_SIZE);
-        channel.join(InetAddress.getByName(MULTICAST_GROUP), NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
-    }
-
-    public void enviarMensaje(String mensaje) throws IOException {
-        bufferEmisor.clear();
-        bufferEmisor.put(mensaje.getBytes());
-        bufferEmisor.flip();
-        channel.send(bufferEmisor, serverSocketAddress);
-    }
-
-    public String recibirMensaje() throws IOException {
-        bufferReceptor.clear();
-        SocketAddress senderAddress = channel.receive(bufferReceptor);
-
-        if (senderAddress != null) {
-            bufferReceptor.flip();
-            byte[] respuestaBytes = new byte[bufferReceptor.remaining()];
-            bufferReceptor.get(respuestaBytes);
-            return new String(respuestaBytes, StandardCharsets.UTF_8);
-        } else {
-            return null;
-        }
-    }
-
-    public void salir() throws IOException {
-        channel.close();
+    public Cliente(String servidorDireccion, String grupoMulticast) throws IOException {
+        this.UDP = new UDP(servidorDireccion, grupoMulticast);
+        this.UDP.registrarObservador(this);
     }
 
     @Override
     public void run() {
-        int c = 0;
-        while (true) {
-            try {
-                String msg = this.recibirMensaje();
-                if(msg != null) {
-                    c++;
-                    System.out.println(msg + " ["+c+"]");
-                }
-                Thread.sleep(30);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        Thread servidorUDP = new Thread(this.UDP);
+        servidorUDP.start();
+        while (servidorUDP.isAlive());
+    }
+
+    @Override
+    public byte[] recibirMensaje() {
+        return new byte[0];
+    }
+
+    @Override
+    public void enviarMensaje(byte[] data) {
+        this.UDP.enviarMensaje(data);
+    }
+
+    @Override
+    public void actualizarEstado(byte[] data) {
+        String msg = new String(data).trim();
+        System.out.println("Desde UDP = "+msg);
     }
 }

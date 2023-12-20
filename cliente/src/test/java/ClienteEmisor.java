15f21e6f859bad622b01b57efaaf86970249f245
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-20 17:07:14 -0300

    Actualización de Clases Unitarias.

--- cliente/src/test/java/ClienteEmisor.java
+++ cliente/src/test/java/ClienteEmisor.java
@@ -3,19 +3,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ClienteEmisor {
     private Cliente cliente;
     @Before
     public void setUp() throws IOException {
-        cliente = new Cliente("26.204.82.158"); // Agregar tu ip públic o de radmin
+        cliente = new Cliente("26.204.82.158","234.0.1.1");
         new Thread(cliente).start();
     }
     @Test
-    public void comunicarAlServidor() throws IOException, InterruptedException {
+    public void comunicarAlServidor() throws InterruptedException {
         while(true){
             String mensaje = "el mensaje";
-            cliente.enviarMensaje("Soy coito "+mensaje);
+            cliente.enviarMensaje(mensaje.getBytes(StandardCharsets.UTF_8));
             Thread.sleep(2500);
         }
     }

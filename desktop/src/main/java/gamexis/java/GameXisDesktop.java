c35d55619fc12dc07de5c237d2272ddb97c8aa2b
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-22 15:25:32 -0300

    Para ejecutar el juego.

--- desktop/src/main/java/gamexis/java/GameXisDesktop.java
+++ desktop/src/main/java/gamexis/java/GameXisDesktop.java
@@ -1,17 +1,28 @@
 package gamexis.java;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
-import starfishgame.LevelScreen;
-import starfishgame.StarFishGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import soylaverga.shooter.CuadritosMoqueteros;
+import soylaverga.shooter.utils.GameConstants;
 
 public class GameXisDesktop {
+
 	public static void main (String[] args) {
 		/*LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new GameXis(), config);*/
 
-		Game myGame = new StarFishGame();
-		new LwjglApplication(myGame, "Starfish Collector v1.2 ",800,600);
+		//Game myGame = new StarFishGame();
+		//new LwjglApplication(myGame, "Starfish Collector v1.2 ",800,600);
+
+
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = GameConstants.SCREEN_WIDTH;
+		config.height = GameConstants.SCREEN_HEIGHT;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 60;
+		config.resizable = false;
+
+		new LwjglApplication(new CuadritosMoqueteros(), config);
 	}
 }

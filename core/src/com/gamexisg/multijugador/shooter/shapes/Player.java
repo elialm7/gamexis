fb51f6ba70f81ec70464af8cd8c7f6dab4270f08
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-27 21:57:02 -0300

    Utilización y renderización del sprite de la tortuga.

--- core/src/com/gamexisg/multijugador/shooter/shapes/Player.java
+++ core/src/com/gamexisg/multijugador/shooter/shapes/Player.java
@@ -1,17 +1,23 @@
 package com.gamexisg.multijugador.shooter.shapes;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 
-public class Player {
 
-	private float size;
+public class Player {
+	private final float size;
+
+	private final SpriteBatch batch = new SpriteBatch();
+	private final Texture img = new Texture("turtle-1.png");
 	private Vector2 position;
 	private int id = -1;
 	private int health;
 
-	private Vector2 center;
+	private final Vector2 center;
 
 	public Player(float x, float y, float size) {
 		this.position = new Vector2(x, y);
@@ -20,14 +26,17 @@ public class Player {
 		center = new Vector2(x, y);
 	}
 
-	public void render(ShapeRenderer sr) {
+	public void render(ShapeRenderer sr, Matrix4 cameraCombined) {
 		sr.rect(position.x, position.y, size, size);
 		sr.end();
 		sr.begin(ShapeType.Filled);
 		sr.rect(position.x, position.y, this.health / 2.0f, size);
 		sr.end();
+		batch.setProjectionMatrix(cameraCombined);
+		batch.begin();
+		batch.draw(img,position.x-2,position.y-5,55,60);
+		batch.end();
 		sr.begin(ShapeType.Line);
-
 		center.x = position.x + size / 2;
 		center.y = position.y + size / 2;
 	}

52ee0c05f6ecf9d4395419a8e09b71a12672e3d2
DDelvRoj <ddelvallerojas@gmail.com>
2023-12-30 01:02:40 -0300

    Uso de BaseGame del custom-framework en la clase que corre el juego.

--- core/src/com/gamexisg/multijugadorconframework/shooter/CuadritosMoqueteros.java
+++ core/src/com/gamexisg/multijugadorconframework/shooter/CuadritosMoqueteros.java
@@ -1,12 +1,14 @@
 package com.gamexisg.multijugadorconframework.shooter;
 
-import com.badlogic.gdx.ApplicationAdapter;
+
+import GameLoop.BaseGame;
 import com.badlogic.gdx.Gdx;
-import com.gamexisg.multijugadorconframework.states.State;
 import com.gamexisg.multijugadorconframework.states.StateController;
 import org.apache.log4j.Logger;
 
-public class CuadritosMoqueteros extends ApplicationAdapter {
+import static com.gamexisg.multijugadorconframework.states.State.StateEnum.MENU_STATE;
+
+public class CuadritosMoqueteros extends BaseGame {
 	private static Logger logger = Logger.getLogger(CuadritosMoqueteros.class);
 	private StateController sc;
 
@@ -18,21 +20,25 @@ public class CuadritosMoqueteros extends ApplicationAdapter {
 	public void create() {
 
 		sc = new StateController(inetAddress);
-		sc.setState(State.StateEnum.MENU_STATE);
+		sc.setState(MENU_STATE);
+
 
 	}
 
 	@Override
 	public void render() {
 
-		sc.render();
+		sc.render(Gdx.graphics.getDeltaTime());
 		sc.update(Gdx.graphics.getDeltaTime());
 	}
 
+
+
 	@Override
 	public void dispose() {
-		super.dispose();
 		sc.dispose();
+		super.dispose();
+
 	}
 
 }

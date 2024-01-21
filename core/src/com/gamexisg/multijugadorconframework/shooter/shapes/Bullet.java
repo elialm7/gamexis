35d4640fac3afe466125254f4862a90fef51f001
DDelvRoj <ddelvallerojas@gmail.com>
2024-01-21 17:29:30 -0300

    Actualización de Bullet para coincidir con los datos del server. Posiblemente lo modificaré más tarde.

--- core/src/com/gamexisg/multijugadorconframework/shooter/shapes/Bullet.java
+++ core/src/com/gamexisg/multijugadorconframework/shooter/shapes/Bullet.java
@@ -5,13 +5,15 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Bullet {
 
+	private int id;
 	private Vector2 position;
 	private float size;
 	private boolean visible = true;
 
-	public Bullet(float x, float y, float size) {
+	public Bullet(int id, float x, float y, float size) {
 		this.position = new Vector2(x, y);
 		this.size = size;
+		setId(id);
 	}
 
 	public void render(ShapeRenderer sr) {
@@ -20,6 +22,14 @@ public class Bullet {
 
 	public Vector2 getPosition() {
 		return position;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public void setPosition(Vector2 position) {
